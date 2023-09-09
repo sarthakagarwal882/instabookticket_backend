0848b9950eac3f218638f5ac4e5e066d4bf8ce76
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-09 16:13:21 +0530

    some minor changes

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ client.connect();
 auth(app);
 
 
-function pingLink() {
-  const linkToPing = 'https://instabook.onrender.com/'; // Replace with the link you want to ping
-let data=axios.get(linkToPing)
-data.then(res=>{})
-}
+// function pingLink() {
+//   const linkToPing = 'https://instabook.onrender.com/login'; // Replace with the link you want to ping
+// let data=axios.get(linkToPing)
+// data.then(res=>{})
+// }
 
-// Ping the link every 10 minutes (10 minutes = 600,000 milliseconds)
-const pingInterval = 10*60*1000;
-setInterval(pingLink, pingInterval);
+// // Ping the link every 10 minutes (10 minutes = 600,000 milliseconds)
+// const pingInterval = 10*60*1000;
+// setInterval(pingLink, pingInterval);
