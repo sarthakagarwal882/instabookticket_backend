a172ab3fc89204b58d1c7fda62979ef3203e7758
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-09 23:27:50 +0530

    axios added in index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const axios=require('axios')
 //js files
 const auth = require('./auth');
 
