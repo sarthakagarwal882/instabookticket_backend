9a974d7ca143ea39acc1a3d915c6cf5f3aaa6553
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-10 11:17:05 +0530

    cors update final

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-//NPM packages
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const axios = require('axios')
-//js files
 const auth = require('./auth');
-
-//Code.
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://instabookticket.netlify.app']
-}));
-
-app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use(cors({
+    origin: ['https://instabookticket.netlify.app','http://localhost:5173'],
+    optionsSuccessStatus: 200
+}))
+
+
 let port = process.env.PORT
 app.listen(8000 || port);
 
