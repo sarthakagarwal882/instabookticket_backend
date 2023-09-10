f37761547764b27ff7093f1125ea59626e55ab5d
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-10 10:47:37 +0530

    cors updated

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const axios=require('axios')
+const axios = require('axios')
 //js files
 const auth = require('./auth');
 
 //Code.
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173/', 'https://instabookticket.netlify.app/']
+    origin: ['https://localhost:5173/', 'https://instabookticket.netlify.app/']
 }));
+
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 let port = process.env.PORT
@@ -23,7 +31,7 @@ app.get('/', (res) => { })
 
 function pingLink() {
     const linkToPing = 'https://instabook-backend.onrender.com'; // Replace with the link you want to ping
-    let data = axios.get(linkToPing+'/')
+    let data = axios.get(linkToPing + '/')
     data.then(res => { })
 }
 
